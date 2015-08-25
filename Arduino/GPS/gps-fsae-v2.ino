fc6e7eeca658a844f6704dc78825dad696f1e420
fredericoasbrasil <frederico.brasil@cear.ufpb.br>
2015-08-25 16:13:27 -0300

    Update gps-fsae-v2.ino

--- Arduino/GPS/gps-fsae-v2.ino
+++ Arduino/GPS/gps-fsae-v2.ino
@@ -10,22 +10,33 @@
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 
-
-//RXPin ligado ao pino TX do GPS
-//TXPin ligado ao pino RX do GPS
+//GPS 5V
+//RXPin ligado ao pino TX do GPS 1
+//TXPin ligado ao pino RX do GPS 1
 static const int RXPin = 4, TXPin = 3;
 static const uint32_t GPSBaud = 4800;
 
+
+//GPS 3.3V
+//RXPin ligado ao pino TX do GPS 2
+//TXPin ligado ao pino RX do GPS 2
+static const int RXPin2 = 7, TXPin2 = 6;
+static const uint32_t GPSBaud2 = 4800;
+
+
 // O objeto TinyGPS++
 TinyGPSPlus gps;
+TinyGPSPlus gps2;
 
 // A coneão serial com o modulo
 SoftwareSerial ss(RXPin, TXPin);
+SoftwareSerial ss2(RXPin2, TXPin2);
 
 void setup() {
 
   Serial.begin(115200);
   ss.begin(GPSBaud);
+  ss2.begin(GPSBaud2);
   Serial.println("Formula UFPB - Sensoriamento");
   Serial.println("Demonstrando leitura de campos NMEA utilizando a biblioteca TinyGPS++");
   Serial.println("Formato dos campos gravados no cartao SD e transmissao por Xbee");
@@ -55,10 +66,34 @@ void loop()
     Serial.print(",");
     Serial.print(gps.satellites.value()); // Quantidade de satelites rastreados
     Serial.println("###");
-  }*/
+  }
+  
+  
+    if (gps2.satellites.isUpdated())
+  {
+    Serial.print("$$$");
+    Serial.print(gps2.date.value()); // Data no formato DDMMYY
+    Serial.print(",");
+    Serial.println(gps2.time.value()); // Hora no formato HHMMSSCC
+    Serial.print(",");
+    Serial.print(gps2.speed.kmph()); // Velocidade instantanea em Km/h
+    Serial.print(",");
+    Serial.print(gps2.location.lat(), 6); // Latitude em graus
+    Serial.print(",");
+    Serial.print(gps2.location.lng(), 6); // Longitude em graus
+    Serial.print(",");
+    Serial.print(gps2.altitude.meters());  // Altitude em metros
+    Serial.print(",");
+    Serial.print(gps2.satellites.value()); // Quantidade de satelites rastreados
+    Serial.println("$$$");
+  }
+  
 
   while (ss.available() > 0)
     gps.encode(ss.read());
+    
+  while (ss2.available() > 0)
+    gps2.encode(ss2.read());    
     
     
       //Codigo de debug, imprime o valor RAW obtido no console. Usar apenas para testes
